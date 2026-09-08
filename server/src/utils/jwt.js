f0c9759fbd7f8d4d28fc64d99ca8eebc33dde756
mrjaiupadhyay <mrjaiupadhyay@gmail.com>
2026-09-08 09:09:47 +0530

    Create jwt.js

---
+++ server/src/utils/jwt.js
@@ -0,0 +1,12 @@
+import jwt from "jsonwebtoken";
+
+export const signToken = (user) =>
+  jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
+    expiresIn: "7d"
+  });
+import jwt from "jsonwebtoken";
+
+export const signToken = (user) =>
+  jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
+    expiresIn: "7d"
+  });
